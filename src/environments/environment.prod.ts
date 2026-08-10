77295c123fb396182fd83cd985901aec071db771
Laxman-Male <laxmanmale122@gmail.com>
2026-08-10 22:05:06 +0530

    Update CORS header in HandleLoginState

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: point this at the deployed backend URL once one exists.
-  apiUrl: 'http://localhost:8000'
+  apiUrl: 'https://progess-study-backend.onrender.com'
 };
